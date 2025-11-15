7ef4f49d64d3323c38c66fec236f081cab70ca62
Rodrigo Lemos <rlemos@exacta.inf.puc-rio.br>
2025-11-15 15:36:10 -0300

    feat: HealthCheck route

---
+++ src/main/java/com/vega/warehouse/api/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.vega.warehouse.api.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public String ping() {
+        return "Api is running";
+    }
+}
