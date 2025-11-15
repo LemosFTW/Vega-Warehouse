fe97b315083f87c3c15c194166426565a09f8bb5
Rodrigo Lemos <rlemos@exacta.inf.puc-rio.br>
2025-11-15 18:08:08 -0300

    feat: Wrapper for api responses

---
+++ src/main/java/com/vega/warehouse/api/dto/response/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.vega.warehouse.api.dto.response;
+
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+
+    public ApiResponse(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
